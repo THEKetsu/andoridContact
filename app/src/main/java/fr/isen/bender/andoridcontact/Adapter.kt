e985e1fd4fdf0a6f556718c11967f27eac3651ba
THEKetsu <quentin.bender@isen.yncrea.fr>
2023-01-27 13:22:29 +0100

    Initial commit

---
+++ app/src/main/java/fr/isen/bender/andoridcontact/Adapter.kt
@@ -0,0 +1,12 @@
+package fr.isen.bender.andoridcontact
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class Adapter {
+
+}
